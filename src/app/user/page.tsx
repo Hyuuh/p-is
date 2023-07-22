e628d730ed27dd37a542192ab591a42ce6c7edcb
Hyuuh <lilhyuuh@gmail.com>
2023-07-22 02:35:45 -0400

    feat: remove react components

--- src/app/user/page.tsx
+++
@@ -1,8 +0,0 @@
-import FormUser from './components/FormUser'
-export default function Home() {
-  return (
-    <main className='flex min-h-screen flex-col items-center justify-center gap-10'>
-      <FormUser />
-    </main>
-  )
-}
